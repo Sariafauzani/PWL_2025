8926f6b32269dadd8d0d35703ffe5cbcaecf59a4
Sariafauzani <ajas2654@gmail.com>
2025-03-05 14:23:53 +0700

    Praktikum 1 - langkah 14

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/user/{name}', function ($name)
+Route::get('/user/{name?}', function ($name=null)
     {return 'Nama saya ' . $name;
 });
