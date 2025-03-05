8a5c8c31ee8295971088b8b23f3b934eb953d831
Sariafauzani <ajas2654@gmail.com>
2025-03-05 13:39:34 +0700

    Praktikum 1 - langkah 12

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/posts/{post}/comments/{comment}', function ($posId, $commentId)
+    {return 'Pos ke-' . $posId . " Komentar ke-" . $commentId;
 });
